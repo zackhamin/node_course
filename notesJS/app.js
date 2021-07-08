c470972c6e9965ebc0dcc736bf811f8bae6ce54f
zackhamin <ishaqhamin@gmail.com>
2021-07-08 21:09:46 +0100

    Command line argv notes

--- notesJS/app.js
+++ notesJS/app.js
@@ -1,6 +1,9 @@
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
-console.log(msg)
+const command = process.argv[2]
 
-console.log(process.argv)
+if (command === "add") {
+    console.log('Adding note..')
+} else if (command === 'remove'){
+    console.log('Removing note')
+}
