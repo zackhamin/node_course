be0c8205754b089e38e0693eebf70ddac9f71c71
zackhamin <ishaqhamin@gmail.com>
2021-07-17 13:19:18 +0100

    Added yargs commands

--- notesJS/app.js
+++ notesJS/app.js
@@ -4,6 +4,23 @@ const yargs = require('yargs')
 // const command = process.argv[2]
 
 console.log(process.argv)
+
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log("New note added")
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Removed note',
+    handler: function() {
+        console.log('Removing notes')
+    }
+})
 console.log(yargs.argv)
 
 // if (command === "add") {
