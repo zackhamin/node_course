98d247574fbb13c0f1f8be9d1f4e8567ae032577
zackhamin <ishaqhamin@gmail.com>
2021-07-18 10:55:20 +0100

    Using fs file system with JSON

--- playground/json.js
+++ playground/json.js
@@ -1,10 +1,23 @@
-const book = {
-    title: 'Goblet of fire',
-    author: 'Harry Putter'
-}
+const fs = require('fs')
+// const book = {
+//     title: 'Goblet of fire',
+//     author: 'Harry Putter'
+// }
 
-const bookJson = JSON.stringify(book)
-console.log(bookJson)
+// const bookJson = JSON.stringify(book)
+// fs.writeFileSync('bookjson.json',bookJson)
 
-const bookParse = JSON.parse(bookJson)
-console.log(bookParse)
+// const dataBuffer = fs.readFileSync('bookjson.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data)
+
+const dataBuffer = fs.readFileSync('bookjson.jsonc')
+const dataJSON = dataBuffer.toString()
+const changedData = JSON.parse(dataJSON)
+changedData.name = "Zack"
+changedData.age = 1352
+const newObject = JSON.stringify(changedData)
+fs.writeFileSync('bookjson.jsonc',newObject)
+
+console.log(changedData)
