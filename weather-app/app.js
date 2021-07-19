91ba74471dffcb67b7caed4d1f0aad5eb86cce8b
zackhamin <ishaqhamin@gmail.com>
2021-07-19 22:47:43 +0100

    Made API request data returning

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,1 +1,8 @@
 const request = require('postman-request')
+
+const url = 'http://api.weatherstack.com/current?access_key=eec85e3060c6743049f9ee58abbf936b&query=Rochdale'
+
+request({ url: url }, (error,response) => {
+    const data = JSON.parse(response.body)
+    console.log(data)
+})
