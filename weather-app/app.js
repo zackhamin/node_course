e28c270298570120aef1834feb9984fa9435ac97
zackhamin <ishaqhamin@gmail.com>
2021-07-20 09:59:13 +0100

    Tidied response json and returning data

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,7 @@ const request = require('postman-request')
 
 const url = 'http://api.weatherstack.com/current?access_key=eec85e3060c6743049f9ee58abbf936b&query=Rochdale'
 
-request({ url: url }, (error,response) => {
-    const data = JSON.parse(response.body)
-    console.log(data)
+request({ url: url, json: true }, (error,response) => {
+    console.log(response.body.current)
+    console.log(`${response.body.current.weather_descriptions[0]}.It is currently ${response.body.current.temperature} degrees out, it feels like ${response.body.current.feelslike} degress out`)
 })
