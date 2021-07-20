5bdcaa01a514526775893776f1accb926bc47061
zackhamin <ishaqhamin@gmail.com>
2021-07-20 17:34:29 +0100

    Added error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,12 +6,23 @@ const url = 'http://api.weatherstack.com/current?access_key=eec85e3060c6743049f9
 const place = 'Los Angeles'
 const mapURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?access_token=${tokenMapBox}`
 request({url:mapURL, json: true}, (error,response) => {
-    const latitude = response.body.features[0].center[1]
-    const longitude = response.body.features[0].center[0]
-    console.log(latitude, longitude)
+    if (error) {
+        console.log("Network error")
+    } else if(response.body.features.length === 0) {
+        console.log("User input error")
+    } else {
+       const latitude = response.body.features[0].center[1]
+       const longitude = response.body.features[0].center[0]
+       console.log(latitude, longitude)
+    }
 })
 
-// request({ url: url, json: true }, (error,response) => {
-//     console.log(response.body.current)
-//     console.log(`${response.body.current.weather_descriptions[0]}.It is currently ${response.body.current.temperature} degrees out, it feels like ${response.body.current.feelslike} degress out`)
-// })
+request({ url: url, json: true }, (error,response) => {
+    if (error) {
+        console.log("Network error")
+    } else if(response.body.error) {
+        console.log("User input error")
+    } else {
+        console.log(`${response.body.current.weather_descriptions[0]}.It is currently ${response.body.current.temperature} degrees out, it feels like ${response.body.current.feelslike} degress out`)
+    }
+})
