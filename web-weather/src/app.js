111cc590b452281b52710215ef2767e7e4e80baa
zackhamin <ishaqhamin@gmail.com>
2021-07-21 22:43:10 +0100

    Sorted

---
+++ web-weather/src/app.js
@@ -0,0 +1,18 @@
+const exp = require('constants')
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname,'../public')))
+
+app.get('/weather', (req, res) =>{
+    res.send({
+        weather: "Sunny",
+        location: "Rochdales"
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Listening to you breathe")
+}) 
