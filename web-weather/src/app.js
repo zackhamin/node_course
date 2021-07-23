c0cddea7baff895b7cdee89499f19f4d50d0ffd3
zackhamin <ishaqhamin@gmail.com>
2021-07-23 17:51:43 +0100

    Added JS and HBS modules

--- web-weather/src/app.js
+++ web-weather/src/app.js
@@ -3,7 +3,6 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-
 app.use(express.static(path.join(__dirname,'../public')))
 
 app.get('/weather', (req, res) =>{
