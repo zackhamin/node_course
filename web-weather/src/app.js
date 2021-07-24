03a114fad23e17dd9e00df7f33118a60955fc808
zackhamin <ishaqhamin@gmail.com>
2021-07-24 10:19:00 +0100

    Added routes and view engine plus templates route

--- web-weather/src/app.js
+++ web-weather/src/app.js
@@ -2,8 +2,33 @@ const exp = require('constants')
 const express = require('express')
 const path = require('path')
 
+//Define path for express config
 const app = express()
-app.use(express.static(path.join(__dirname,'../public')))
+const publicPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates')
+
+//Set up handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicPath))
+
+app.get('',(req,res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Ishaq Amin'
+    })
+})
+
+app.get('/about',(req,res) => {
+    res.render('about')
+})
+
+app.get('/help',(req,res) => {
+    res.render('help')
+})
+
 
 app.get('/weather', (req, res) =>{
     res.send({
