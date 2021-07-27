9aee2d67c4ef6c2c245e321c8cd8dc68bd9adf89
zackhamin <ishaqhamin@gmail.com>
2021-07-27 21:43:50 +0100

    Added form submit for location submission

--- web-weather/public/js/app.js
+++ web-weather/public/js/app.js
@@ -1,1 +1,19 @@
-console.log("Javascript")
+const weatherForm = document.querySelector('form')
+const searchTerm = document.querySelector('input')
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = searchTerm.value
+    fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
+        res.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+            console.log(data)
+         } })
+    }).catch((err) => {
+        console.log(err)
+    })
+})
